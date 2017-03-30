a7496e19ed367164157c709f393a63a74f0b28ba
Google1234 <taoJiang_ict@163.com>
2017-03-30 22:19:49 +0800

    v1 support train, fix tf.concat ,slim.losses bugs

--- slim/train_image_classifier.py
+++ slim/train_image_classifier.py
@@ -469,10 +469,10 @@ def main(_):
       # Specify the loss function #
       #############################
       if 'AuxLogits' in end_points:
-        tf.losses.softmax_cross_entropy(
+        slim.losses.softmax_cross_entropy(
             logits=end_points['AuxLogits'], onehot_labels=labels,
             label_smoothing=FLAGS.label_smoothing, weights=0.4, scope='aux_loss')
-      tf.losses.softmax_cross_entropy(
+      slim.losses.softmax_cross_entropy(
           logits=logits, onehot_labels=labels,
           label_smoothing=FLAGS.label_smoothing, weights=1.0)
       return end_points
