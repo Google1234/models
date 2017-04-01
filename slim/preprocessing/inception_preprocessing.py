c5a1344fa17e15a60dd9aa3ab61614c9c0a77052
Google1234 <taoJiang_ict@163.com>
2017-04-01 14:34:53 +0800

    V2.2 amend preprocess_for_eval

--- slim/preprocessing/inception_preprocessing.py
+++ slim/preprocessing/inception_preprocessing.py
@@ -262,6 +262,12 @@ def my_preprocess_for_train(image, height, width, bbox,
     3-D float Tensor of distorted image used for training with range [-1, 1].
   """
   with tf.name_scope(scope, 'distort_image', [image, height, width, bbox]):
+    #check image has been precessd,image height shoule== width   
+    shape=tf.shape(image)
+    if tf.equal(shape[0], shape[1], name=None)==False:           
+        print ("image height!= width,check whether image has been proprocessd")
+        raise Exception
+
     if bbox is None:
       bbox = tf.constant([0.0, 0.0, 1.0, 1.0],
                          dtype=tf.float32,
@@ -352,7 +358,49 @@ def preprocess_for_eval(image, height, width,
     image = tf.subtract(image, 0.5)
     image = tf.multiply(image, 2.0)
     return image
+def my_preprocess_for_eval(image, height, width, scope=None):
+  """Prepare one image for evaluation.
 
+  If height and width are specified it would output an image with that size by
+  applying resize_bilinear.
+
+  If central_fraction is specified it would cropt the central fraction of the
+  input image.
+
+  Args:
+    image: 3-D Tensor of image. If dtype is tf.float32 then the range should be
+      [0, 1], otherwise it would converted to tf.float32 assuming that the range
+      is [0, MAX], where MAX is largest positive representable number for
+      int(8/16/32) data type (see `tf.image.convert_image_dtype` for details)
+    height: integer
+    width: integer
+    central_fraction: Optional Float, fraction of the image to crop.
+    scope: Optional scope for name_scope.
+  Returns:
+    3-D float Tensor of prepared image.
+  """
+  with tf.name_scope(scope, 'eval_image', [image, height, width]):
+    tf.summary.image('source_image',tf.expand_dims(image, 0))
+    if image.dtype != tf.float32:
+      image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    # Crop the central region of the image with an area containing 87.5% of
+    # the original image.
+    #check image has been precessd,image height shoule== width
+    shape=tf.shape(image)
+    if tf.equal(shape[0], shape[1], name=None)==False:
+        print ("image height!= width,check whether image has been proprocessd")
+        raise Exception 
+    
+    if height and width: 
+      # Resize the image to the specified height and width.
+      image = tf.expand_dims(image, 0)
+      image = tf.image.resize_bilinear(image, [height, width],
+                                       align_corners=False)
+      image = tf.squeeze(image, [0])
+    image = tf.subtract(image, 0.5)
+    image = tf.multiply(image, 2.0)
+    tf.summary.image('processed_image',tf.expand_dims(image, 0))
+    return image
 
 def preprocess_image(image, height, width,
                      is_training=False,
@@ -380,4 +428,4 @@ def preprocess_image(image, height, width,
   if is_training:
     return my_preprocess_for_train(image, height, width, bbox, fast_mode)
   else:
-    return preprocess_for_eval(image, height, width)
+    return my_preprocess_for_eval(image, height, width)
